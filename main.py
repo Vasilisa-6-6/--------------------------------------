88648e280b35ef0e90de57dd458879f7f47966d7
Vasilisa <277248801+Vasilisa-6-6@users.noreply.github.com>
2026-09-25 15:25:20 +0500

    +новые фунции

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ bot = telebot.TeleBot(TOKEN)
 
 engin = pyttsx3.init()
 
+he = ['/start', '/help', '/weather', '/F', '/C']
+
 def get_weather(city):
     url = f"https://wttr.in/{city}?format=%C+%t"
     responce = requests.get(url)
@@ -22,7 +24,12 @@ def speak(text):
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    bot.send_message(message.chat.id, "Привет! Я бот, который озвучивает прогноз погоды")
+    bot.send_message(message.chat.id, "Привет! Я бот, который озвучивает прогноз погоды и не только😏")
+@bot.message_handler(commands=["help"])
+def start(message):
+    bot.send_message(message.chat.id, "Вот мои команды:")
+    for i in he:
+        bot.send_message(message.chat.id, i)
 
 @bot.message_handler(commands=["weather"])
 def weather(message):
@@ -35,6 +42,27 @@ def weather(message):
     answer = f"Погода в {city}: {weather_info}"
     bot.send_message(message.chat.id,  answer)
     speak(answer)
-
+@bot.message_handler(commands=["F"])
+def weather(message):
+    words = message.text.split(maxsplit=1)
+    if len(words) < 2:
+        bot.send_message(message.chat.id, "Пожалуйста, укажи сколько градусов по Фаренгейту перевести в градусы по Цельсию. Пример: /F 32.0")
+        return
+    f = float(words[1])
+    temp = (f - 32) * (5/9)
+    answer = f"Температура в градусах по Цельсию: {temp}"
+    bot.send_message(message.chat.id,  answer)
+    speak(answer)
+@bot.message_handler(commands=["C"])
+def weather(message):
+    words = message.text.split(maxsplit=1)
+    if len(words) < 2:
+        bot.send_message(message.chat.id, "Пожалуйста, укажи сколько градусов по Цельсию перевести в градусы по Фаренгейту. Пример: /С 2")
+        return
+    c = float(words[1])
+    temp = (c * (9/5)) + 32 
+    answer = f"Температура в градусах по Фаренгейту: {temp}"
+    bot.send_message(message.chat.id,  answer)
+#    speak(answer) :(
 
 bot.polling()
